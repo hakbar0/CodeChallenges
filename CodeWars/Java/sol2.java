02acd70d9ee23e6402edb9eb3dc11002f40d73c8
hakbar0 <haseeb2000@hotmail.com>
2024-02-10 20:32:47 +0000

    another solution

---
+++ CodeWars/Java/sol2.java
@@ -0,0 +1,12 @@
+public class CodeWars {
+  public static int strCount(String str, char letter) {
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) != letter) {
+        continue;
+      }
+      count++;
+    }
+    return count;
+  }
+}
